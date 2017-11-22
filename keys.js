c4333fdc8091b239dee0274e9d4e98eb1892c334
Tia Vasquez <tiavasquez3@gmail.com>
2017-11-22 08:51:38 -0800

    initial push

---
+++ keys.js
@@ -0,0 +1,15 @@
+console.log('this is loaded');
+
+var twitterKeys = {
+  consumer_key: 'duaxgQrKpGrBPK9gwBtYRPWEi',
+  consumer_secret: 'LnSPOihS2e3CzjDaLfj97UEwbsEZzECaHSyD7MpIEGxgZ4h8u4',
+  access_token_key: '2337369834-QwPrxmuJze2jE4IfktEbQeNu9CnYnsqsVcvXa9W',
+  access_token_secret: 'Oa6WcRRCBNQenhikjqO3JQXr96Ecj4BWbpRBCUhd2rybS',
+}
+
+module.exports = twitterKeys;
+
+//spotify client id: 5b329d31ecbf4edb94f1f440bc2f0bc4
+//spotify client secret: daa829ae018b4f709096998c3ea119a0
+
+//OMDB API requires an API key. You may use `40e9cece`.
---
+++ liri.js
@@ -0,0 +1,89 @@
+
+//get the 3rd argument which will be one of the following:
+//`my-tweets` OR `spotify-this-song` OR `movie-this` OR `do-what-it-says`
+var userCommand = process.argv[2];
+
+switch(userCommand) {
+  case "my-tweets":
+    myTweets();
+    break;
+  case "spotify-this-song":
+    spotifyThisSong();
+    break;  
+  case "movie-this":
+    movieThis();
+    break;
+  case "do-what-it-says":
+    doWhatItSays();
+    break;  
+  default:
+  //code block
+}
+
+function myTweets() {
+
+  // get twitter keys from keys.js
+  var twitterKeys = require('./keys.js');
+  console.log("Twitter consumer_key is: " + twitterKeys.consumer_key);
+
+} //end myTweets function
+
+function spotifyThisSong() {
+
+  // use fs Node package for reading and writing files
+  var fs = require("fs");
+  // read text from random.txt and place in the variable "data"
+  fs.readFile("random.txt", "utf8", function(error, data) {
+
+    // If the code experiences any errors it will log the error to the console.
+    if (error) {
+      return console.log(error);
+    }
+
+    // We will then print the contents of data
+    console.log(data);
+
+    // Then split it by commas (to make it more readable)
+    var dataArr = data.split(",");
+    for (var i = 0; i < dataArr.length; i++) {
+      dataArr[i] = dataArr[i].trim();
+    }
+
+    // We will then re-display the content as an array for later use.
+    console.log(dataArr);
+
+  }); //end fs.readFile
+
+} //end spotifyThisSong function
+
+function movieThis() {
+
+  // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
+  var request = require("request");
+
+  // Grab the movieName which will always be the third node argument.
+  var movieName = process.argv[2];
+
+  // Then run a request to the OMDB API with the movie specified
+  var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+  // This line is just to help us debug against the actual URL.
+  console.log(queryUrl);
+
+  request(queryUrl, function(error, response, body) {
+
+    // If the request is successful
+    if (!error && response.statusCode === 200) {
+
+      // Parse the body of the site and recover just the imdbRating
+      // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+      console.log("Release Year: " + JSON.parse(body).Year);
+    }
+  });
+
+  
+} //end movieThis function
+
+function doWhatItSays() {
+  
+} //end doWhatItSays function
